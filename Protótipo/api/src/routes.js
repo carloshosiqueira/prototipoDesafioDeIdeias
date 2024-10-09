3ae20ae141f125a12e64a7174ba6d7d3d7d1192f
carloshosiqueira <140626781+carloshosiqueira@users.noreply.github.com>
2024-10-09 10:37:08 -0300

    delete pasta antiga

--- Protótipo/api/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const Entrega = require('./controllers/entrega')
-
-router.get('/entrega', Entrega.read)
-
-module.exports = router
